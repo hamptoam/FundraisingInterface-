0aba628298deaded65dd15beb8c2f145c5fface4
hamptoam <ameliabedelia94@gmail.com>
2019-08-27 08:40:52 -0500

    Initial commit of docs.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fundraising_Capstone.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fundraising_Capstone.Startup))]
+namespace Fundraising_Capstone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
