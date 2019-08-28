b566838fee4ffe8b9eff6b6cb74271ea3fb72c89
hamptoam <ameliabedelia94@gmail.com>
2019-08-28 08:52:22 -0500

    Added Api keys in own folder and adding folder to .gitignore.

---
+++ APIKeys/APIKeys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fundraising_Capstone.APIKeys
+{
+    public class APIKeys
+    {
+
+        public static string sID = "ACad990750b705f73b388ec579ef0a82ba";
+
+        public static string theToken = "723839759b7f1707f018d937cb1305c4";
+
+    }
+}
