f3cbd9c5d55f3824fad8bad1425c195d619a1fcc
hamptoam <ameliabedelia94@gmail.com>
2019-09-05 11:10:30 -0500

    Added controllers and views, need unhandled exception error fixed.

--- Startup.cs
+++ Startup.cs
@@ -23,32 +23,32 @@ namespace Fundraising_Capstone2
             var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
 
 
-            // In Startup iam creating first Admin Role and creating a default Admin User    
-            if (!roleManager.RoleExists("Admin"))
-            {
+            //// In Startup iam creating first Admin Role and creating a default Admin User    
+            //if (!roleManager.RoleExists("Admin"))
+            //{
 
-                // first we create Admin rool   
-                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
-                role.Name = "Admin";
-                roleManager.Create(role);
+            //    // first we create Admin rool   
+            //    var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+            //    role.Name = "Admin";
+            //    roleManager.Create(role);
 
-                //Here we create a Admin super user who will maintain the website                  
+            //    //Here we create a Admin super user who will maintain the website                  
 
-                var user = new ApplicationUser();
-                user.UserName = "shanu";
-                user.Email = "syedshanumcain@gmail.com";
+            //    var user = new ApplicationUser();
+            //    user.UserName = "hamptoam";
+            //    user.Email = "lurdeyquirks@outlook.com";
 
-                string userPWD = "A@Z200711";
+            //    string userPWD = "@MeL!a";
 
-                var chkUser = UserManager.Create(user, userPWD);
+            //    var chkUser = UserManager.Create(user, userPWD);
 
-                //Add default User to Role Admin   
-                if (chkUser.Succeeded)
-                {
-                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+            //    //Add default User to Role Admin   
+            //    if (chkUser.Succeeded)
+            //    {
+            //        var result1 = UserManager.AddToRole(user.Id, "Admin");
 
-                }
-            }
+            //    }
+            //}
 
             // creating Creating Manager role    
             if (!roleManager.RoleExists("Manager"))
