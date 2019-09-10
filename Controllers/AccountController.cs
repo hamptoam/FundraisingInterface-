0e7677a0b8d074f8e9126c490b71b9a8511ce08b
hamptoam <ameliabedelia94@gmail.com>
2019-09-10 13:48:46 -0500

    Fixed register task in accountcontroller to fix routing issues.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -161,14 +161,22 @@ namespace Fundraising_Capstone2.Controllers
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                    await this.UserManager.AddToRoleAsync(user.Id, model.UserRoles);
 
-                    return RedirectToAction("Index", "Home");
+                    if (this.User.IsInRole("Employee"))
+                    {
+                        return RedirectToAction("Phone", "Index");
+                    }
+                    else if (this.User.IsInRole("Manager"))
+                    {
+                        return RedirectToAction("Managers", "Index");
+                    }
                 }
                 AddErrors(result);
             }
