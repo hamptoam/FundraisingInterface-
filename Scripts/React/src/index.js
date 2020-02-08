886457d1432a6aa6dc35dcbd9833c7d3c8cb20de
hamptoam <ameliabedelia94@gmail.com>
2020-02-08 12:51:06 -0600

    rearranging folders to fix loading issue.

--- Scripts/React/src/index.js
+++ Scripts/React/src/index.js
@@ -1,6 +1,7 @@
 ﻿import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Login from "./react/components/login";
+import "babel-loader";
 
 
 ReactDOM.render(<Login />, document.getElementById("login_page"));
