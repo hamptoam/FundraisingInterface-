23e9ce1fd0b008aceeb224991676765b28fddc9f
hamptoam <ameliabedelia94@gmail.com>
2020-02-07 22:19:44 -0600

    Updated dev-dependancies, still figuring out rendering view for login.

---
+++ Scripts/src/React/components/login.js
@@ -0,0 +1,27 @@
+﻿import React, { Component } from 'react';
+class Login extends Component {
+    render() {
+        return (
+            <form className="form-horizontal">
+                <div className="form-group">
+                    <label className="col-md-2 control-label">Email</label>
+                    <div className="col-md-10">
+                        <input type="text" name="email" class="form-control" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label className="col-md-2 control-label">Password</label>
+                    <div className="col-md-10">
+                        <input type="password" name="password" className="form-control" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="col-md-offset-2 col-md-10">
+                        <input type="submit" value="Log in" className="btn btn-default" />
+                    </div>
+                </div>
+            </form>
+        );
+    }
+}
+export default Login;
---
+++ Scripts/src/index.js
@@ -0,0 +1,7 @@
+﻿import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Login from "./react/components/login";
+
+
+ReactDOM.render(<Login />, document.getElementById("login_page"));
+
