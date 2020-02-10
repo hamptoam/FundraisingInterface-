5ebce98764f246472092dab3b8e8c814d09fe0e2
hamptoam <ameliabedelia94@gmail.com>
2020-02-09 21:20:48 -0600

    Fatal error reverting for timesake, trying to reset to old branch.

--- Scripts/React/src/index.js
+++
@@ -1,8 +0,0 @@
-﻿import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import Login from "./react/components/login";
-import "babel-loader";
-
-
-ReactDOM.render(<Login />, document.getElementById("login_page"));
-
